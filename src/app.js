c2071e890eecf795ec0fdf030c127fe0683dee1e
Sahil5046 <sahilsk03860@gmail.com>
2024-04-16 22:57:38 +0530

    user control set up

--- src/app.js
+++ src/app.js
@@ -8,7 +8,15 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static('public'))
+app.use(cookieParser())
+
+//router import
+import userRouter from './routes/user.routes.js'
+
+//router decleration
+app.use("/api/v2/users", userRouter)
 
 export { app } 
